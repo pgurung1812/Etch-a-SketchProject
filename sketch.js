d9cfc2154be7f3a8020100185fef461245391ad3
Prasanna <pgurung1@my.hpu.edu>
2024-05-24 22:58:46 -0500

    added final feature of User Selection of grids

--- sketch.js
+++ sketch.js
@@ -1,19 +1,38 @@
 const container=document.querySelector("#container");
+const userclick=document.querySelector("#user");
 
-for(let i=0;i<20;i++){
-    for( let j=0;j<20;j++)
+
+userclick.addEventListener("click",function(e){
+    let choice=prompt("please enter a number between 1 and 100 to create a grid");
+
+    if(choice >=1 && choice<=100)
         {
-                const div=document.createElement("div");
-                div.classList.add("square");
-                div.addEventListener("mouseover", function(e){
-                   e.target.style.background="red";
-                });
-                container.appendChild(div);
+
+            for(let i=0;i<choice;i++){
+                for( let j=0;j<choice;j++)
+                    {
+                        console.log(container);
+                            const div=document.createElement("div");
+                            div.classList.add("square");
+                            div.addEventListener("mouseover", function(e){
+                               e.target.style.background="red";
+                            });
+                            
+                            div.style.width=`${804/choice}px`;
+                            div.style.height=`${804/choice}px`;
+                            
+                            container.appendChild(div);
+                        
+                        }
+                       
+                        
             
-            }
-           
-            
+                    }
+
 
         }
+});
+
+
     
     
